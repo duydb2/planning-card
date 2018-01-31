1970db6da6209f583da46c1751b3c52c46183292
Johannes Stein <johannesstein@freeze-dev.com>
2018-01-31 01:46:17 +0100

    Put some structure in place

--- Card.js
+++ Card.js
@@ -1,28 +1,56 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const Card = ({ children }) => (<View style={styles.container}><View style={styles.innerContainer}><Text style={styles.text}>{children}</Text></View></View>);
+const Card = ({ children }) => (
+  <View style={styles.container}>
+    <View style={styles.innerContainer}>
+      <View style={styles.circle}>
+        <Text style={styles.circleText}>{children}</Text>
+      </View>
+      <Text style={styles.text}>{children}</Text>
+    </View>
+  </View>
+);
 
 const styles = StyleSheet.create({ container: {
   flex: 1,
-  paddingTop: 32,
-  paddingBottom: 32,
-  paddingLeft: 20,
-  paddingRight: 20,
-  backgroundColor: 'blue'
+  paddingTop: 40,
+  paddingBottom: 40,
+  paddingLeft: 24,
+  paddingRight: 24,
+  backgroundColor: 'rgb(66, 122, 130)'
 },
 innerContainer: {
   flex: 1,
   alignItems: 'center',
   justifyContent: 'center',
-  borderWidth: 8,
+  borderWidth: 4,
   borderRadius: 16,
-  borderColor: 'rgb(10, 10, 129)',
+  borderColor: 'rgb(14, 70, 78)',
   backgroundColor: 'white'
+},
+circle: {
+  width: 60,
+  height: 60,
+  borderRadius: 30,
+  backgroundColor: 'rgb(1, 46, 52)',
+  position: 'absolute',
+  top: 4,
+  left: 4,
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderWidth: 2,
+  borderColor: 'rgb(105, 150, 156)'
+},
+circleText: {
+  color: 'rgb(105, 150, 156)',
+  fontSize: 32,
+  fontWeight: 'bold'
 },
 text: {
   fontSize: 240,
   fontWeight: 'bold',
+  color: 'rgb(1, 46, 52)'
 } });
 
 export default Card;
---
+++ colors.js
@@ -0,0 +1,5 @@
+export const primaryLighter = 'rgb(105, 150, 156)';
+export const primaryLight = 'rgb(66, 122, 130)';
+export const primary = 'rgb(36, 96, 104)';
+export const primaryDark = 'rgb(14, 70, 78)';
+export const primaryDarker = 'rgb(1, 46, 52)';
